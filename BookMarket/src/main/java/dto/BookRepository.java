83ebdc6221ff4e76d18e12aa41ed2139544a6f16
Jaydevlee <yousick5@gmail.com>
2025-11-07 11:11:12 +0900

    북마켓 자바빈즈 추가

--- BookMarket/src/main/java/dto/BookRepository.java
+++ BookMarket/src/main/java/dto/BookRepository.java
@@ -1,5 +1,16 @@
 package dto;
 
-public class BookRepository {
+import java.util.ArrayList;
+import dto.Book;
 
+public class BookRepository {
+	private ArrayList<Book> listOfBooks = new ArrayList<Book>();
+	
+	public BookRepository() {
+		Book book1 = new Book("ISBN1234", "C# 프로그래밍", 27000);
+		book1.setAuthor("우재남");
+		book1.setDescription("설명설명설명설명설명");
+		book1.setPublisher("한빛아카데미");
+		book1.setCategory("IT모바일");
+	}
 }
